744315396a71a35f20365839b230aebf820c6cf6
Ondřej Láznička <get@ondrejlaznicka.cz>
2026-03-02 14:04:01 +0100

    Update GraphQLPersistedQueriesProvider.php

--- src/GraphQLPersistedQueriesProvider.php
+++ src/GraphQLPersistedQueriesProvider.php
@@ -1,13 +1,13 @@
 <?php
 namespace BrainzStudios\GraphQLPersistedQueries;
 
+use BrainzStudios\GraphQLPersistedQueries\Middleware\PersistedFragmentsMiddleware;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\Router;
-class GraphQLPersistedQueriesProvider {
+class GraphQLPersistedQueriesProvider extends ServiceProvider{
 
 
     public function boot(Router $router) {
-        $router->aliasMiddleware('persisted_fragments', \BrainzStudios\GraphQLPersistedQueries\Middleware\PersistedFragmentsMiddleware::class);
-
+        $router->aliasMiddleware('persisted_fragments', PersistedFragmentsMiddleware::class);
     }
 }
