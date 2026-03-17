a6d1cdc6803ef157249a8c818ef9c5584c37561c
Ondřej Láznička <get@ondrejlaznicka.cz>
2026-03-17 11:42:13 +0100

    Add support for mutation handling in PersistedQueryMiddleware

--- src/Middleware/PersistedQueryMiddleware.php
+++ src/Middleware/PersistedQueryMiddleware.php
@@ -19,7 +19,15 @@ class PersistedQueryMiddleware {
         }
 
         $safeName = preg_replace('/[^a-zA-Z0-9]/', '', $operationName);
-        $path = resource_path("graphql/queries/{$safeName}.graphql");
+
+        // Try mutations first, then queries
+        $path = resource_path("graphql/mutations/{$safeName}.graphql");
+        $type = 'mutations';
+
+        if (!file_exists($path)) {
+            $path = resource_path("graphql/queries/{$safeName}.graphql");
+            $type = 'queries';
+        }
 
         if (!file_exists($path)) {
             return response()->json(['errors' => [['message' => 'Query not whitelisted']]], 403);
@@ -45,6 +53,32 @@ class PersistedQueryMiddleware {
                         $addedFragments[$fName] = true;
                         $foundNew = true;
                         Log::info("Fragment $fName PRIPOJEN (i pro vnořené potřeby).");
+                    }
+                }
+            }
+        }
+
+        // Also check for mutations fragments if this is a mutation
+        if ($type === 'mutations') {
+            $mutationsFragmentsPath = resource_path("graphql/mutations/fragments");
+            if (File::isDirectory($mutationsFragmentsPath)) {
+                $fragmentFiles = File::allFiles($mutationsFragmentsPath);
+                $addedFragments = [];
+                $foundNew = true;
+
+                while ($foundNew) {
+                    $foundNew = false;
+                    foreach ($fragmentFiles as $file) {
+                        $fName = $file->getBasename('.graphql');
+                        if (isset($addedFragments[$fName])) continue;
+                        $fragmentContent = file_get_contents($file->getPathname());
+                        $pattern = '/\.\.\.\s*' . preg_quote($fName, '/') . '\b/';
+                        if (preg_match($pattern, $queryBody)) {
+                            $queryBody .= "\n" . $fragmentContent;
+                            $addedFragments[$fName] = true;
+                            $foundNew = true;
+                            Log::info("Mutation fragment $fName PRIPOJEN.");
+                        }
                     }
                 }
             }
